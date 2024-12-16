e15e38be302665281f78ce50e7ce0db744bf8696
João Miguel <joaomiguelmorais1108@gmail.com>
2024-12-16 19:50:28 -0300

    Update parte 2.py

--- parte 2.py
+++ parte 2.py
@@ -19,5 +19,5 @@ leão.falar()
 lobo.falar()
 
 
-9)
-Quando dois objetos de classes diferentes possuem métodos com o mesmo nome, cada método será executado de acordo com a implementação de sua respectiva classe. Isso caracteriza o polimorfismo, onde o mesmo nome de método (falar) realiza ações distintas dependendo do objeto que o invoca.
+'''9)
+Quando dois objetos de classes diferentes possuem métodos com o mesmo nome, cada método será executado de acordo com a implementação de sua respectiva classe. Isso caracteriza o polimorfismo, onde o mesmo nome de método (falar) realiza ações distintas dependendo do objeto que o invoca.'''
